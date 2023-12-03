4bb635d87f463d1b863f52a194c6c8bc851c3841
XJS <git@xjs.net.cn>
2023-12-03 17:52:24 +0800

    update: 可以调试&测试

--- test/test.c
+++ test/test.c
@@ -3,18 +3,19 @@
 char s[100];
 int main()
 {
-	//setbuf(stdout, NULL);
-	int row=7,col=7;
-    puts("READY\n");
+	// setbuf(stdout, NULL);
+	int row = 7, col = 7;
+	puts("READY\n");
 	fflush(stdout);
 	gets(s);
-	if(strcmp(s,"BLACK")==0)
+	if (strcmp(s, "BLACK") == 0)
 		printf("MOVE(1,1)\n");
 	fflush(stdout);
-    while(1){
-		scanf(" MOVE(%d,%d)",&row,&col);
-		printf("MOVE(%d,%d)\n",row,col>14?1:col+1);
-	fflush(stdout);
+	while (1)
+	{
+		scanf(" MOVE(%d,%d)", &row, &col); // scanf的空格很重要，不然会读到换行符等
+		printf("MOVE(%d,%d)\n", row, col > 14 ? 1 : col + 1);
+		fflush(stdout);
 	}
-    return 0;
+	return 0;
 }
