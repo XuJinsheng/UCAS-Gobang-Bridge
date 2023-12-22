ab6fb548a583d0a629ad9cb24b8b581dc71e0de2
Xujinsheng <git@xjs.net.cn>
2023-12-22 08:57:40 +0800

    update doc

--- test/test.c
+++ test/test.c
@@ -3,18 +3,24 @@
 char s[100];
 int main()
 {
-	// setbuf(stdout, NULL);
-	int row = 7, col = 7;
-	puts("READY\n");
-	fflush(stdout);
-	gets(s);
-	if (strcmp(s, "BLACK") == 0)
-		printf("MOVE(1,1)\n");
+	puts("READY\n");			 // 输出READY表示初始化完成
+	fflush(stdout);				 // puts和printf之后都要刷新缓冲区
+	gets(s);					 // 读取自己的颜色
+	if (strcmp(s, "BLACK") == 0) // 判断自己是黑方还是白方
+		printf("MOVE(1,1)\n");	 // 如果是黑方，就先下一步
 	fflush(stdout);
 	while (1)
 	{
-		scanf(" MOVE(%d,%d)", &row, &col); // scanf的空格很重要，不然会读到换行符等
-		printf("MOVE(%d,%d)\n", row, col > 14 ? 1 : col + 1);
+		int row = 7, col = 7;
+		// 读取对手下棋的坐标，注意scanf的空格很重要，不然会读到换行符等
+		scanf(" MOVE(%d,%d)", &row, &col);
+
+		// 然后根据对手的坐标下一步棋，这里只是简单的下在对手的右边
+		row = row;
+		col = col > 14 ? 1 : col + 1;
+
+		// 输出自己的下棋坐标
+		printf("MOVE(%d,%d)\n", row, col);
 		fflush(stdout);
 	}
 	return 0;
